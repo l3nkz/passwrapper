a1a71612dcd79b019e88998cbf2e71288323f24f
Till Smejkal <till.smejkal+passwrapper@ossmail.de>
2015-05-07 16:48:41 +0200

    Add help messages and other useful functionality

--- passwrapper.sh
+++ passwrapper.sh
@@ -36,17 +36,91 @@ passwrapper_initialize() {
     return ${EC}
 }
 
+lspass_help() {
+    printf "lspass [OPTIONS]\n"
+    printf "  Show all password stores available.\n\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message\n"
+    printf "      --passwords   Also show all passwords in the stores\n"
+}
+
 lspass() {
-    printf "Available password stores:\n"
+    local show_passwords=0
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                lspass_help
+                return 0
+                ;;
+            --passwords)
+                show_passwords=1
+                ;;
+            *)
+                printf "Unknown option: %s\n\n" $1
+                lspass_help
+                return 1
+                ;;
+        esac
+
+        shift
+    done
+
     for f in ${PASS_HOME}/*; do
         if [ -d $f ]; then
-            printf "%s\n" $(basename $f)
+            if [ $show_passwords -eq 1 ]; then
+                printf "%s:\n" $(basename $f)
+                (PASSWORD_STORE_DIR=$f pass ls)
+                printf "\n"
+            else
+                printf "%s\n" $(basename $f)
+            fi
         fi
     done
 }
 
+mkpass_help() {
+    printf "mkpass [OPTIONS] NAME\n"
+    printf "  Create a new password store with the name NAME.\n\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message\n"
+    printf "      --with-key    Also generate a new PGP-Key for\n"
+    printf "                    the password store\n"
+}
+
 mkpass() {
-    local name=$1
+    # Parse the given arguments
+    if [ $# -lt 1 ]; then
+        mkpass_help
+        return 1
+    fi
+
+    local generate_key=0
+    local name=
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                mkpass_help
+                return 0
+                ;;
+            --with-key)
+                generate_key=1
+                ;;
+            *)
+                if [ -z "$name" ]; then
+                    name=$1
+                else
+                    printf "Unknown option: %s\n\n" $1
+                    mkpass_help
+                    return 1;
+                fi
+                ;;
+        esac
+
+        shift
+    done
+    
     local pass_path=${PASS_HOME}/${name}
 
     if [ -e ${pass_path} ]; then
@@ -56,10 +130,53 @@ mkpass() {
 
     # Create the new password store
     mkdir -p ${pass_path}
+
+    # If specified also create the PGP key
+    if [ ${generate_key} -eq 1 ]; then
+        gpg --full-gen-key
+    fi
+}
+
+rmpass_help() {
+    printf "rmpass [OPTIONS] NAME\n"
+    printf "  Remove the password store with the name NAME.\n\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message\n"
+    printf "      --quiet       Do not ask any questions\n"
 }
 
 rmpass() {
-    local name=$1
+    local name=
+    local quiet=0
+
+    if [ $# -lt 1 ]; then
+        rmpass_help
+        return 1
+    fi
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                rmpass_help
+                return 0
+                ;;
+            --quiet)
+                quiet=1
+                ;;
+            *)
+                if [ -z "$name" ]; then
+                    name=$1
+                else
+                    printf "Unknown option: %s\n\n" $1
+                    rmpass_help
+                    return 1
+                fi
+                ;;
+        esac
+
+        shift
+    done
+
     local pass_path=${PASS_HOME}/${name}
 
     if [ ! -e ${pass_path} ]; then
@@ -67,30 +184,66 @@ rmpass() {
         return 1
     fi
 
-    printf "Every password stored in %s will be lost.\n" ${name}
-    printf "Are you sure you want to remove? [y/N] "
-    read response
+    if [ $quiet -eq 0 ]; then
+        printf "Every password stored in %s will be lost.\n" ${name}
+        printf "Are you sure you want to remove? [y/N] "
+        read response
 
-    case "${response}" in
-        [yY])
-            # Remove the corresponding password store
-            rm -rf ${pass_path}
+        case "${response}" in
+            [yY])
+                ;;
+            [nN]|*)
+                return 0
+                ;;
+        esac
+    fi
 
-            # If the just removed password store was currently active also
-            # deactivate it.
-            if [ "${pass_path}" = "${PASS_DIR}" ]; then
-                passoff
-            fi
+    # Remove the corresponding password store
+    rm -rf ${pass_path}
 
-            ;;
-        [nN]|*)
-            return 1
-            ;;
-    esac
+    # If the just removed password store was currently active also
+    # deactivate it.
+    if [ "${pass_path}" = "${PASS_DIR}" ]; then
+        passoff
+    fi
+}
+
+passon_help() {
+    printf "passon [OPTIONS] NAME\n"
+    printf "  Activate the password store with the name NAME.\n\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message\n"
 }
 
 passon() {
-    local name=$1
+    local name=
+
+    if [ $# -lt 1 ]; then
+        passon_help
+        return 1
+    fi
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                rmpass_help
+                return 0
+                ;;
+            *)
+                if [ -z "$name" ]; then
+                    name=$1
+                else
+                    printf "Unknown option: %s\n\n" $1
+                    passon_help
+                    return 1
+                fi
+                ;;
+        esac
+
+        shift
+    done
+
+
     local pass_path=${PASS_HOME}/${name}
 
     if [ ! -e ${pass_path} ]; then
@@ -102,7 +255,31 @@ passon() {
     export PASS_DIR=${pass_path}
 }
 
+passoff_help() {
+    printf "passoff [OPTIONS]\n"
+    printf "  Deactivate the password stores.\n\n"
+    printf "Options:\n"
+    printf "  -h, --help        Show this help message\n"
+
+}
+
 passoff() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                rmpass_help
+                return 0
+                ;;
+            *)
+                printf "Unknown option: %s\n\n" $1
+                passoff_help
+                return 1
+                ;;
+        esac
+
+        shift
+    done
+
     unset PASSWORD_STORE_DIR
     unset PASS_DIR
 }
